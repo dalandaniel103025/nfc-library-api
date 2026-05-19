8d779bcfb182cb211e700546d8f0043e800f0af6
dalandaniel103025 <markdanielp.sabedra@gmail.com>
2026-05-20 05:25:05 +0800

    Diagnostic

--- db.php
+++ db.php
@@ -1,21 +1,9 @@
 <?php
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
-$database = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+echo "HOST: ".getenv("MYSQLHOST")."<br>";
+echo "USER: ".getenv("MYSQLUSER")."<br>";
+echo "DATABASE: ".getenv("MYSQLDATABASE")."<br>";
+echo "PORT: ".getenv("MYSQLPORT")."<br>";
 
-$conn = new mysqli(
-    $host,
-    $user,
-    $password,
-    $database,
-    $port
-);
-
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-}
+exit();
 ?>
